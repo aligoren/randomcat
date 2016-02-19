f8e8bd56d57ddabdb6f6c1f9c88bd01a932d4132
aligoren <goren.ali@yandex.com>
2016-02-19 17:23:28 +0200

    Add Cats :)

---
+++ assets/cute.js
@@ -0,0 +1,35 @@
+function get_cat() {
+    var randomFoldNum = Math.floor(Math.random()*5) + 1;
+    var randomNumber;
+
+    if(randomFoldNum == 1) {
+    	randomNumber = Math.floor(Math.random()*100) + 1;
+    } else if(randomFoldNum == 2) {
+    	randomNumber = Math.floor(Math.random()*34) + 1;
+    } else if(randomFoldNum == 3) {
+    	randomNumber = Math.floor(Math.random()*46) + 1;
+    } else if(randomFoldNum == 4) {
+    	randomNumber = Math.floor(Math.random()*59) + 1;
+    } else if(randomFoldNum == 5) {
+    	randomNumber = Math.floor(Math.random()*89) + 1;
+    }
+    document.body.innerHTML += "<div id='cats'><h1>CLICK IMAGE FOR REFRESH</h1><img onclick='reload_cat();' src='" + randomFoldNum + "/" + randomNumber + ".jpg' /></div>";
+}
+
+function reload_cat() {
+    var randomFoldNum = Math.floor(Math.random()*5) + 1;
+    var randomNumber;
+
+    if(randomFoldNum == 1) {
+    	randomNumber = Math.floor(Math.random()*100) + 1;
+    } else if(randomFoldNum == 2) {
+    	randomNumber = Math.floor(Math.random()*34) + 1;
+    } else if(randomFoldNum == 3) {
+    	randomNumber = Math.floor(Math.random()*46) + 1;
+    } else if(randomFoldNum == 4) {
+    	randomNumber = Math.floor(Math.random()*59) + 1;
+    } else if(randomFoldNum == 5) {
+    	randomNumber = Math.floor(Math.random()*89) + 1;
+    }
+    document.getElementById('cats').innerHTML = "<h1>CLICK IMAGE FOR REFRESH</h1><img onclick='reload_cat();' src='" + randomFoldNum + "/" + randomNumber + ".jpg' />";
+}
